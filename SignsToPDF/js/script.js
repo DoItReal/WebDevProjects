03b167324178652bf97125715d7c82425741e5bb
DoItReal <66225172+DoItReal@users.noreply.github.com>
2023-05-18 16:13:45 +0300

    update

--- SignsToPDF/js/script.js
+++ SignsToPDF/js/script.js
@@ -1,20 +1,9 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var canv;
-var ctx;
-var sign;
 window.onload = function init() {
-    canv = document.getElementById('myCanvas');
-    ctx = canv.getContext('2d');
-    init_canvas();
+    //canv = document.getElementById('myCanvas');
+    // ctx = canv.getContext('2d');
     createPDF();
+    // Invoke the function to generate the PDF
+    // generatePDF();
 };
 /* 0 (��������)
  * ������� ������� (bulgarian)
@@ -24,6 +13,11 @@ window.onload = function init() {
  *
  * */
 class Sign {
+    width;
+    height;
+    content;
+    canvas;
+    ctx;
     constructor(w, h) {
         this.width = w;
         this.height = h;
@@ -39,7 +33,10 @@ class Sign {
     generate() {
         let rows = [this.content.alergens, this.content.name.bg, this.content.name.en, this.content.name.de, this.content.name.rus];
         this.ctx.save();
-        let step = this.height / rows.length - 20;
+        //border
+        this.ctx.fillStyle = "lightgray";
+        this.ctx.fillRect(0, 0, this.width, this.height);
+        let step = (this.height / rows.length) - 10;
         for (let i = 0; i < rows.length; i++) {
             let x = this.width / 20;
             let y = step * (i + 1);
@@ -51,59 +48,89 @@ class Sign {
     }
 }
 class SignContent {
+    alergens;
+    name;
     constructor(alergens, name) {
         this.alergens = alergens;
         this.name = name;
     }
 }
-function createPDF() {
-    return __awaiter(this, void 0, void 0, function* () {
-        //creates new PDF Document
-        const doc = yield PDFLib.PDFDocument.create();
-        //adds page to just created PDF Document
-        /*          Method doc.addPage(pageSize(w,h))
-         * 1 - (w,h) < <number>, <number> >  ... doc.addPage(700,800);
-         * 2 - (PageSizes.*) ... doc.addPage(PageSizes.A4);
-         * 3
-         * */
+async function createPDF() {
+    //creates new PDF Document
+    const doc = await PDFLib.PDFDocument.create();
+    //adds page to just created PDF Document
+    /*          Method doc.addPage(pageSize(w,h))
+     * 1 - (w,h) < <number>, <number> >  ... doc.addPage(700,800);
+     * 2 - (PageSizes.*) ... doc.addPage(PageSizes.A4);
+     * 3
+     *
+     */
+    //  const page = doc.addPage(PDFLib.PageSizes.A4);
+    const data = [
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 1', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 2', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 3', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 4', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 5', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 6', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 7', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 8', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 9', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 10', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { bg: "%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8", en: 'Translation 11', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        // Add more entries as needed
+    ];
+    const chunks = [];
+    for (let i = 0; i < data.length; i += 8) {
+        chunks.push(data.slice(i, i + 8));
+    }
+    // Generate pages for each chunk of entries
+    for (const chunk of chunks) {
         const page = doc.addPage(PDFLib.PageSizes.A4);
-        sign = new Sign(290, 280);
-        sign.setContent(new SignContent("1, 2, 6, 9", { bg: decodeURI("%D0%9F%D1%8A%D1%80%D0%B6%D0%B5%D0%BD%D0%B8%20%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%84%D0%B8"), en: "French Fries", de: "deutsch", rus: decodeURI("%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%88%D0%BA%D0%B8%20%D0%A4%D1%80%D0%B8") }));
-        sign.generate();
-        //creates JPEG image from canvas 
-        var jpgImage = yield doc.embedJpg(sign.canvas.toDataURL('image/jpeg'));
-        document.body.appendChild(sign.canvas);
-        //draws just created JPEG image to the page
-        page.drawImage(jpgImage, {
-            x: 0,
-            y: 0,
-            width: sign.width,
-            height: sign.height
-        });
-        //saving the PDF doc as dataUri
-        const pdfDataUri = yield doc.saveAsBase64({ dataUri: true });
-        //adding the PDF document ot the DOM
-        var framePDF = document.getElementById('pdf');
-        framePDF.src = pdfDataUri;
-    });
+        const { width, height } = page.getSize();
+        let y = height - 5;
+        let x = 0;
+        // Print each entry on the page
+        for (const entry of chunk) {
+            const signsInPage = 8;
+            let sign = new Sign(width / 2 - 10, height / (signsInPage / 2) - 10);
+            sign.setContent(new SignContent(entry.numbers.join(', '), { bg: decodeURI(entry.bg), en: entry.en, de: "deutsch", rus: decodeURI("%D0%9A%D0%B0%D1%80%D1%82%D0%BE%D1%88%D0%BA%D0%B8%20%D0%A4%D1%80%D0%B8") }));
+            sign.generate();
+            //creates JPEG image from canvas 
+            var jpgImage = await doc.embedJpg(sign.canvas.toDataURL('image/jpeg'));
+            //used for debugging
+            document.body.appendChild(sign.canvas);
+            //draws just created JPEG image to the page
+            page.drawImage(jpgImage, {
+                x: x + 5,
+                y: y - sign.height,
+                width: sign.width,
+                height: sign.height
+            });
+            console.log(y);
+            if (x > 0) {
+                x = 0;
+                y -= sign.height + 5;
+                console.log(height + ' ' + width);
+            }
+            else
+                x = x + sign.width + 5;
+        }
+    }
+    //saving the PDF doc as dataUri
+    const pdfDataUri = await doc.saveAsBase64({ dataUri: true });
+    //adding the PDF document ot the DOM
+    var framePDF = document.getElementById('pdf');
+    framePDF.src = pdfDataUri;
 }
-//Creating content for printing
-function init_canvas() {
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(0, 0);
-    ctx.fillStyle = "cyan";
-    ctx.rect(0, 0, 120, 25);
-    ctx.fill();
-    ctx.beginPath();
-    ctx.font = "20px Helvetica bold italic";
-    ctx.fillStyle = "black";
-    ctx.fillText("Missle War", 15, 20);
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 25, 120, 40);
-    ctx.font = "25px Arial bold";
-    ctx.fillStyle = "black";
-    ctx.fillText("The Game!", 5, 55);
-    ctx.restore();
-}
+/*
+// Save the PDF document to a file
+const pdfBytes = await pdfDoc.save();
+
+// Use the pdfBytes to save the PDF document or perform further operations
+
+// Example: Save the PDF to a file
+const fs = require('fs');
+fs.writeFileSync('output.pdf', pdfBytes);
+*/
 //# sourceMappingURL=script.js.map
