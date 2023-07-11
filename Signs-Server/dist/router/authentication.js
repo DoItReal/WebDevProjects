5aa8fcf4b9db6a78ad6afcb37fe5c8c1ea44d95b
DoItReal <66225172+DoItReal@users.noreply.github.com>
2023-07-11 23:29:32 +0300

    Delete Signs-Server directory

--- Signs-Server/dist/router/authentication.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//import { login, register } from '../controllers/authentication';
-exports.default = (router) => {
-    //router.post('/auth/register', register);
-    // router.post('/auth/login', login);
-};
-//# sourceMappingURL=authentication.js.map
